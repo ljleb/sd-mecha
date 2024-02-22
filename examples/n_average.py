e2a965b8ba60918be1fbc4f9b8f634c302a30e4a
ljleb <lebel.louisjacob@gmail.com>
2024-02-21 19:54:09 -0500

    n_average fp64

--- examples/n_average.py
+++ examples/n_average.py
@@ -14,9 +14,7 @@ models = [
 
 recipe = models[0]
 for i, model in enumerate(models[1:], start=2):
-    # dtype to accommodate precision loss by alpha near 1
-    dtype = torch.float16 if i - 2 < 4 else torch.float32 if i - 2 < 16 else torch.float64
-    recipe = sd_mecha.weighted_sum(model, recipe, alpha=(i-1)/i, dtype=dtype)
+    recipe = sd_mecha.weighted_sum(model, recipe, alpha=(i-1)/i)
 
-merger = sd_mecha.RecipeMerger(models_dir=r"E:\sd\models\Stable-diffusion")
+merger = sd_mecha.RecipeMerger(models_dir=r"E:\sd\models\Stable-diffusion", default_dtype=torch.float64)
 merger.merge_and_save(recipe, output_path="n_average")
