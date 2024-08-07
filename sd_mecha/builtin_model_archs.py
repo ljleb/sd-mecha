4fc97060a1382ba6cfd6f84ded0219c378676c8e
ljleb <lebel.louisjacob@gmail.com>
2024-08-07 17:42:16 -0400

    flux

--- sd_mecha/builtin_model_archs.py
+++ sd_mecha/builtin_model_archs.py
@@ -5,3 +5,4 @@ models_dir = pathlib.Path(__file__).parent / "models"
 register_model_arch(models_dir / "sd1_ldm.yaml", "sd1")
 register_model_arch(models_dir / "sdxl_sgm.yaml", "sdxl")
 register_model_arch(models_dir / "sd3_sgm.yaml", "sd3")
+register_model_arch(models_dir / "flux_flux.yaml", "flux")
