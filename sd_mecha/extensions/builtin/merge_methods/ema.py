e211674048034e6408cc35217ea079f830996294
ljleb <lebel.louisjacob@gmail.com>
2026-03-01 11:05:30 -0500

    generalize exchange_ema

--- sd_mecha/extensions/builtin/merge_methods/ema.py
+++ sd_mecha/extensions/builtin/merge_methods/ema.py
@@ -9,25 +9,29 @@ def exchange_ema(
     ...
 
 
-@merge_method(implements=exchange_ema)
-def exchange_ema_sd1(
-    model: Parameter(StateDict[Tensor], model_config="sd1-ldm"),
-    **kwargs,
-) -> Return(Tensor, model_config="sd1-ldm"):
-    input_keys = model.model_config.keys()
-    target_key = kwargs["key"]
-    to_ema_key_fn = to_ema_key_fns.get(model.model_config.identifier, lambda k: k)
-    ema_key = to_ema_key_fn(target_key)
+def implement_exchange_ema(config, to_ema_key_fn):
+    @merge_method(identifier=f"exchange_ema_'{config}'", implements=exchange_ema)
+    def exchange_ema_impl(
+        model: Parameter(StateDict[Tensor], model_config=config),
+        **kwargs,
+    ) -> Return(Tensor, model_config=config):
+        input_keys = model.model_config.keys()
+        target_key = kwargs["key"]
+        ema_key = to_ema_key_fn(target_key)
 
-    if ema_key in input_keys:
-        return model[ema_key]
+        if ema_key in input_keys:
+            return model[ema_key]
 
-    for input_key in input_keys:
-        if to_ema_key_fn(input_key) == target_key:
-            return model[input_key]
-    return model[target_key]
+        for input_key in input_keys:
+            if to_ema_key_fn(input_key) == target_key:
+                return model[input_key]
+        return model[target_key]
 
 
 to_ema_key_fns = {
     "sd1-ldm": lambda k: f"model_ema.{k[len('model.'):].replace('.', '')}"
 }
+
+
+for config, key_fn in to_ema_key_fns.items():
+    implement_exchange_ema(config, key_fn)
